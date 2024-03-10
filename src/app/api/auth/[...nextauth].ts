5b03c32368973130e1e6153e7b1fa1852355b507
knarimatsu <nariomasahisa@gmail.com>
2024-03-10 18:39:31 +0900

    AuthOptionのKeyの修正 providerではなくproviders

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,11 +1,13 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
-    provider: [
+export const authOption: AuthOptions = {
+    providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
         }),
     ],
 };
+
+export default NextAuth(authOption);
